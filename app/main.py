1ee24fe24324fb903180001bdbb3ab66435bedbf
qwochung <tranquochung0404@gmail.com>
2025-06-29 17:28:35 +0700

    predict multi iamges

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.api.v1.routers import router as api_router
 from app.core.exception_handlers import register_exception_handlers
 
-app = FastAPI(title="Violation Detection API")
+app = FastAPI(title="Violation D/api/v1etection API")
 
 # Đăng ký routers
 app.include_router(api_router, prefix="/api/v1")
